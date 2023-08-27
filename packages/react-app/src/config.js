aea9551d7d7f8768f933957c7e2967fe289fc066
Yajat003 <128574967+Yajat003@users.noreply.github.com>
2023-08-27 14:13:53 +0530

    Update config.js added Router address and Goerli chainId

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,10 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = "0x5F368F25a9b4219BCb89DF9B1890000a5eEadDE9"; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/onhcEGi8ji2DmFzlpBRZB85FHz3UtVvB",
   },
 };
